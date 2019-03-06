6decffa98b01d014bdacce6b56f4f216768eff0b
Rob Davis <robert.lyndon.davis@gmail.com>
2019-03-06 11:01:40 +0000

    - added bulma

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+// Import all the sass
+require('./assets/app.scss');
+
 Vue.config.productionTip = false;
 
 new Vue({
