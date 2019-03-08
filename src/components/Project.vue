33dc46b1144e6fcf11340ad1e61fb3e1f1930ca5
Rob <bfe5e925@opayq.com>
2019-03-08 22:08:48 +0000

    - icon caption fixes

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -4,42 +4,31 @@
     <div class="subtitle has-text-dark has-text-weight-light">@3 days ago</div>
     <div class="subtitle has-text-dark">
         {{ projectDescription }}
-
-        <a href="#" class="has-text-danger" aria-label="Demo" title="Demo">
-          <font-awesome-icon icon="desktop"/>
-        </a>
     </div>
-    <!-- <div class="level is-mobile">
-        <div class="level-left">
-            <span class="is-7 has-text-weight-light">
-                Last update 5 hours ago
-            </span>
-        </div>
-        <div class="level-right">
-            <a href="#" class="level-item button is-danger">Demo</a>
-        </div>
-    </div> -->
     <div class="level is-mobile">
       <div class="level-left">
         <a href="#" class="level-item has-text-info" aria-label="Stars" title="Stars">
           <font-awesome-icon icon="star"/>
-          &nbsp;(5)
+          <span class="is-size-7 has-text-weight-light has-right-nudge">5</span>
         </a>
         <a href="#" class="level-item has-text-info" aria-label="Forks" title="Forks">
           <font-awesome-icon icon="code-branch"/>
-          &nbsp;(1)
+          <span class="is-size-7 has-text-weight-light has-right-nudge">1</span>
         </a>
         <a href="#" class="level-item has-text-info" aria-label="Contributors" title="Contributors">
           <font-awesome-icon icon="users"/>
-          &nbsp;(2)
+          <span class="is-size-7 has-text-weight-light has-right-nudge">2</span>
         </a>
         <a href="#" class="level-item has-text-info" aria-label="Commits" title="Commits">
           <font-awesome-icon icon="upload"/>
-          &nbsp;(50)
+          <span class="is-size-7 has-text-weight-light has-right-nudge">20</span>
         </a>
         <a href="#" class="level-item has-text-info" aria-label="Releases" title="Releases">
           <font-awesome-icon icon="tag"/>
-          &nbsp;(6)
+          <span class="is-size-7 has-text-weight-light has-right-nudge">6</span>
+        </a>
+        <a href="#" class="has-text-danger" aria-label="Demo" title="Demo">
+          <font-awesome-icon icon="desktop"/>
         </a>
       </div>
     </div>
@@ -64,3 +53,9 @@ export default class Project extends Vue {
   public projectLastUpdate!: string;
 }
 </script>
+
+<style scoped>
+  .has-right-nudge {
+    padding-left: 5px;
+  }
+</style>
