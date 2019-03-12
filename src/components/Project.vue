6aa4777cf365ee46b03e6600d8e62ae394075bbb
Andrew Jackson <andrew@a-jackson.co.uk>
2019-03-12 08:48:24 +0000

    add links and remove icons that don't work

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="box">
-    <div class="title has-text-dark">{{ repo.name }}</div>
+    <div class="title has-text-dark"><a :href="repo.html_url">{{ repo.name }}</a></div>
     <div class="subtitle has-text-dark has-text-weight-light">
       <timeago :datetime="repo.updated_at"></timeago>
     </div>
@@ -9,25 +9,13 @@
     </div>
     <div class="level is-mobile">
       <div class="level-left">
-        <a href="#" class="level-item has-text-info" aria-label="Stars" title="Stars">
+        <a :href="repo.html_url + '/stargazers'" class="level-item has-text-info" aria-label="Stars" title="Stars">
           <font-awesome-icon icon="star"/>
           <span class="is-size-7 has-text-weight-light has-right-nudge">{{ repo.stargazers_count }}</span>
         </a>
-        <a href="#" class="level-item has-text-info" aria-label="Forks" title="Forks">
+        <a :href="repo.html_url + '/network/members'" class="level-item has-text-info" aria-label="Forks" title="Forks">
           <font-awesome-icon icon="code-branch"/>
           <span class="is-size-7 has-text-weight-light has-right-nudge">{{ repo.forks }}</span>
-        </a>
-        <a href="#" class="level-item has-text-info" aria-label="Contributors" title="Contributors">
-          <font-awesome-icon icon="users"/>
-          <span class="is-size-7 has-text-weight-light has-right-nudge">2</span>
-        </a>
-        <a href="#" class="level-item has-text-info" aria-label="Commits" title="Commits">
-          <font-awesome-icon icon="upload"/>
-          <span class="is-size-7 has-text-weight-light has-right-nudge">20</span>
-        </a>
-        <a href="#" class="level-item has-text-info" aria-label="Releases" title="Releases">
-          <font-awesome-icon icon="tag"/>
-          <span class="is-size-7 has-text-weight-light has-right-nudge">6</span>
         </a>
         <a :href="repo.homepage" v-if="repo.homepage" class="has-text-danger" aria-label="Demo" title="Demo">
           <font-awesome-icon icon="desktop"/>
