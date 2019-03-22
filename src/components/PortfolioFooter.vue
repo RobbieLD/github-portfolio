f174ec146e3c2a7821d54d5c50c0ceda49053a0c
Rob Davis <rdavis@calrom.com>
2019-03-22 14:57:25 +0000

    -fixing bulma bug in footer

--- src/components/PortfolioFooter.vue
+++ src/components/PortfolioFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <section v-bind:class="heroClass" class="hero is-dark is-medium is-bold">
+  <section class="hero is-dark is-medium is-bold">
     <div class="hero-body">      
         <div class="has-text-centered">Created By <a href="https://github.com/RobbieLD" class="is-text">@RobbieLd</a> and <a href="https://github.com/a-jackson">@a-jackson</a></div>
       </div>
@@ -13,5 +13,6 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 export default class PortfolioFooter extends Vue {}
 </script>
 
-<style>
+<style lang="scss" scoped>
+
 </style>
