abee652bc3e3fb99294eb1d89b58c0941f0618dd
Rob Davis <rdavis@calrom.com>
2019-03-22 16:34:26 +0000

    - updated footer

--- src/components/PortfolioFooter.vue
+++ src/components/PortfolioFooter.vue
@@ -1,8 +1,13 @@
 <template>
   <section class="hero is-dark is-medium is-bold">
-    <div class="hero-body">      
-        <div class="has-text-centered">Created By <a href="https://github.com/RobbieLD" class="is-text">@RobbieLd</a> and <a href="https://github.com/a-jackson">@a-jackson</a></div>
+    <div class="hero-body">
+      <div class="has-text-centered">
+        The
+        <a href="https://github.com/RobbieLD/github-portfolio">GitHub Portfolio</a> Created By
+        <a href="https://github.com/RobbieLD" class="is-text">@RobbieLd</a> and
+        <a href="https://github.com/a-jackson">@a-jackson</a>
       </div>
+    </div>
   </section>
 </template>
 
@@ -14,5 +19,4 @@ export default class PortfolioFooter extends Vue {}
 </script>
 
 <style lang="scss" scoped>
-
 </style>
