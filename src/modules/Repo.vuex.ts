65e6ccdae813cbd057f5805de932c009ce769cc7
Andrew Jackson <andrew@a-jackson.co.uk>
2019-03-27 10:13:16 +0000

    display repositories in chronological order

--- src/modules/Repo.vuex.ts
+++ src/modules/Repo.vuex.ts
@@ -24,6 +24,7 @@ export class RepoStore extends VuexModule {
                 cfgSection.class,
                 rps
                     .filter((r) => cfgSection.repos.includes(r.name))
+                    .sort((a, b) => new Date(b.updated_at).getTime() - new Date(a.updated_at).getTime())
                     .map((repository) => this.config.repositoryOverrides &&
                         this.config.repositoryOverrides[repository.name] ?
                         Object.assign(repository, this.config.repositoryOverrides[repository.name]) : repository)));
